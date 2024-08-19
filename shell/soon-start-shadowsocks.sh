6920d7cf6d70d87835659ecd6b1e1fa3e55767a0
zhanglinxiao <linxiao.zhang@pisell.com>
2024-08-19 11:50:12 +0800

    modify

---
+++ shell/soon-start-shadowsocks.sh
@@ -0,0 +1,7 @@
+
+# yum install docker
+sh /data/www/dnmp/shell/yum-install-docker.sh && /
+
+# start-shadowsocks
+sh /data/www/dnmp/shell/vpn/start-shadowsocks.sh
+
--- shell/step1.sh
+++
@@ -1,9 +0,0 @@
-
-mkdir -p /data/www/ && cd /data/www/ && git clone https://github.com/goodBoysss/dnmp.git
-
-# yum install docker
-sh /data/www/dnmp/shell/yum-install-docker.sh
-
-# start-shadowsocks
-sh /data/www/dnmp/shell/vpn/start-shadowsocks.sh
-
