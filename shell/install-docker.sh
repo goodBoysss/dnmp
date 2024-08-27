7267fc5b2d8d0cbcb2e17614676aa15fdc139019
zhanglinxiao <linxiao.zhang@pisell.com>
2024-08-27 11:07:33 +0800

    modify

--- shell/install-docker.sh
+++ shell/install-docker.sh
@@ -1,8 +1,10 @@
 
-curl -fsSL https://get.docker.com -o get-docker.sh && \
+# sudo sh /data/www/dnmp/shell/install-docker.sh
 
-sudo sh get-docker.sh && \
+sudo curl -fsSL https://get.docker.com -o /data/www/dnmp/shell/get-docker.sh && \
+
+sudo sh /data/www/dnmp/shell/get-docker.sh && \
 
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose & \
 
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
--- shell/step0.sh
+++ shell/step0.sh
@@ -1,9 +1,9 @@
 
-# yum install git
-apt-get install git -y
+# apt-get install git
+sudo apt-get install git -y
 
 # yum install docker and pull dnmp
-sudo mkdir -p /data/www/ && cd /data/www/ && sudo git clone https://github.com/goodBoysss/dnmp.git
+sudo mkdir -p /data/www/ && cd /data/www/ && sudo git clone https://github.com/goodBoysss/dnmp.git && sudo chown ubuntu -R /data/www/
 
 # soon-start-shadowsocks.sh
 sudo sh /data/www/dnmp/shell/soon-start-shadowsocks.sh
